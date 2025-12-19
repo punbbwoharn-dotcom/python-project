3a4f73733775090c46984697cb5db277be7066d6
009ภูมพิพรรธน์ โวหาร <punbbwoharn@gmail.com>
2025-12-19 15:15:26 +0700

    first commit

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+
 num = int(input("ใส่เลขมาไอดำ  : "))
 hund = num // 100
 ten = (num % 100) // 10
 one = num % 10
 total = hund + ten + one
 print("Output :", total)
+
---
+++ test1.py
@@ -0,0 +1,7 @@
+# Simple output
+print("Hello World")
+
+# Variable assignment
+name = "Nora"
+age = 25
+print(f"Hi, my name is {name} and I am {age} years old.")
